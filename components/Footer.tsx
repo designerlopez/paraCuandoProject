3da31eae1a42d5d217155c11354a28fb507c8f6b
edgar_lopez <edgarlopezacevedo@gmail.com>
2023-03-02 17:59:03 -0500

    feat: posicionamiento de lupa en footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,9 +3,9 @@ import Lupa from './svg/Lupa';
 const Footer = () => {
   return (
     <div className="footer flex justify-center bg-[url('../public/images/footer.png')] bg-cover bg-no-repeat bg-center h-screen max-h-80 bottom-0">
-      <div className="m-0  relative flex items-center">
+      <div className="m-0  relative flex items-center w-[350px] sm:w-[435px]">
         <input
-          className="input_text text-left w-full sm:w-96 h-8 bg-white py-2 pr-1 pl-2 rounded-full sm:text-sm placeholder-sm:text-xs"
+          className="input_text text-left w-full  h-[46px] bg-white py-2 pl-5  rounded-full text-sm placeholder-sm:text-xs"
           type="text"
           placeholder="¿Qué quieres ver en tu ciudad?"
         />
