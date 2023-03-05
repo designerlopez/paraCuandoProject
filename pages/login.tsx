07db48dafa39e599292e4cb53a9f478def0aa6ab
edgar_lopez <edgarlopezacevedo@gmail.com>
2023-03-05 17:28:46 -0500

    feat: error en deploy

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,3 +1,21 @@
-export default function Container() {
-  return <div className="login "></div>;
+import SignInOther from '@/components/SignInOther';
+
+export default function () {
+  return (
+    <div
+      className="h-screen bg-cover bg-no-repeat bg-center "
+      style={{
+        backgroundImage: "url('/images/fondoSignIn.png')",
+      }}
+    >
+      <div
+        className="h-full w-full absolute top-0 left-0 bg-gradient-to-r from-black via-transparent to-transparent"
+        style={{ mixBlendMode: 'multiply' }}
+      ></div>
+      <div className="logo bg-[url('../public/images/brand.png')]  bg-center bg-no-repeat w-72 h-52 m-0"></div>
+      <div className="formulario">
+        <SignInOther />
+      </div>
+    </div>
+  );
 }
